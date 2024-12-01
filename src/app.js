31b4da0cc45946b952e3c7b9f2667fe70381bc5b
sthiago <sthiago@gmail.com>
2024-12-01 11:25:54 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,84 @@
+import axios from "axios";
+import { Chess, SQUARES } from "chess.js";
+import { Chessground } from "chessground";
+
+
+function toDests(chess) {
+    const dests = new Map();
+    SQUARES.forEach(s => {
+        const ms = chess.moves({square: s, verbose: true});
+        if (ms.length) dests.set(s, ms.map(m => m.to));
+    });
+    return dests;
+}
+
+function toColor(chess) {
+    return (chess.turn() === 'w') ? 'white' : 'black';
+}
+
+function getWeightedRandomMove(data) {
+    const total = data.moves.reduce((prev, curr) => prev + curr.white + curr.black + curr.draws, 0);
+
+    for (let i = 0; i < data.moves.length; i++) {
+        const move = data.moves[i]
+        move.total = move.white + move.black + move.draws;
+        move.percentage = move.total / total;
+        move.cumulativePercentage = i > 0 ? move.percentage + data.moves[i-1].cumulativePercentage : move.percentage;
+        move.from = move.uci.slice(0, 2);
+        move.to = move.uci.slice(2, 4);
+    }
+
+    const roll = Math.random();
+    let selectedMove;
+    for (const move of data.moves) {
+        if (roll <= move.cumulativePercentage) {
+            selectedMove = move;
+            break;
+        }
+    }
+
+    return selectedMove;
+}
+
+function lichessOpeningPlay(cg, chess, delay, firstMove) {
+    return async (orig, dest) => {
+        chess.move({from: orig, to: dest});
+        const response = await axios.get(`https://explorer.lichess.ovh/lichess?variant=standard&fen=${chess.fen()}&speeds=classical,rapid,blitz&ratings=1600,1800,2000,2200,2500`);
+        const move = getWeightedRandomMove(response.data);
+        if (move !== undefined) {
+            chess.move(move.san);
+            cg.move(move.from, move.to);
+            cg.set({
+                turnColor: toColor(chess),
+                movable: {
+                    color: toColor(chess),
+                    dests: toDests(chess)
+                }
+            });
+            cg.playPremove();
+        } else {
+            console.log("No game found")
+        }
+    };
+}
+
+const chess = Chess();
+const config = {
+    coordinates: false,
+    disableContextMenu: true,
+    movable: {
+        color: "white",
+        free: false,
+        dests: toDests(chess),
+    }
+};
+
+const ground = Chessground(document.getElementById("chessground"), config);
+
+ground.set({
+    movable: {
+        events: {
+            after: lichessOpeningPlay(ground, chess, 1000, false)
+        }
+    }
+})
